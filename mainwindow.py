7680f526728f76d3e151a5ee922651f6ae4564f7
Tyler Rivenburgh <rivenburgh.t@husky.neu.edu>
2017-04-08 23:00:27 -0400

    Update buy button, sell button is fully working

--- mainwindow.py
+++ mainwindow.py
@@ -14,12 +14,10 @@ from PyQt5.QtWidgets import (QApplication, QCheckBox, QGridLayout, QGroupBox,
 
 from yahoo_finance import Share
 
-
-
 # DB Credentials
 hostname = 'localhost'
-username = 'username'
-password = 'password'
+username = 'root'
+password = 'homesweethome'
 database = 'mydb'
 myConnection = pymysql.connect(host=hostname, user=username, passwd=password, db=database, autocommit=True)
 
@@ -135,13 +133,12 @@ class BuyResultWindow(QDialog):
 		self.buywindow.show()
 		return self.buywindow
 
-
 # Window for buying a stock
 class BuyWindow(QDialog):
 	def __init__(self, parent=None):
 		super(BuyWindow, self).__init__(parent)
 		self.grid = QGridLayout()
-		cur_balance = doQuery(myConnection, "SELECT current_balance FROM users WHERE user_id = 2")
+		cur_balance = doQuery(myConnection, "SELECT current_balance FROM users WHERE user_id = 1")
 		cur_balance = str(cur_balance[0][0])
 		self.log = QLineEdit()
 		self.log.setText("Current Balance: '" + cur_balance + "'")
@@ -167,8 +164,6 @@ class BuyWindow(QDialog):
 		# date field
 		cur_date = QDate(datetime.datetime.now())
 		self.date = QDateEdit(cur_date)
-		self.pydate = self.date.date()
-		self.pydate = self.pydate.toPyDate()
 		self.vbox1.addWidget(self.date)
 
 		enterbutton = QPushButton("Enter")
@@ -182,23 +177,145 @@ class BuyWindow(QDialog):
 		ticker = self.textfield.toPlainText()
 		price = self.textfield1.toPlainText()
 		volume = self.textfield2.toPlainText()
-		date = self.pydate
-		date = date.strftime('%Y/%m/%d')
-		cur_balance = doQuery(myConnection, "SELECT current_balance FROM users WHERE user_id = 2")
-		balance = int(cur_balance[0][0]) - (int(price) * int(volume))
+		date = self.date.date()
+		pydate = date.toPyDate()
+		date = pydate.strftime('%Y/%m/%d')
 
-		try:
-			doQuery(myConnection, "INSERT INTO Portfolio (user_id, stock, p_bought_at, volume, d_bought_at)" +
-										"VALUES (2 ,'" + ticker + "', '" + price + "' , '" + volume + "', '" + date + "')")
-			doQuery(myConnection, "UPDATE users SET current_balance = '" + str(balance) + "'WHERE user_id = 2")
+		if doQuery(myConnection, "SELECT stock FROM stocks WHERE stock = '" + ticker + "'"):
+			current_balance = doQuery(myConnection, "SELECT current_balance FROM users WHERE user_id = 1")
+			if current_balance[0][0] > (int(price) * int(volume)):
 
-			self.resultDialog("The stock has been successfully purchased. You're new account balance is: '" + str(balance) + "')")
+				try:
+					balance = int(current_balance[0][0]) - (int(price) * int(volume))
+					doQuery(myConnection, "INSERT INTO Portfolio (user_id, stock, p_bought_at, volume, d_bought_at)" +
+												"VALUES (1 ,'" + ticker + "', '" + price + "' , '" + volume + "', '" + date + "')")
+					doQuery(myConnection, "UPDATE users SET current_balance = '" + str(balance) + "'WHERE user_id = 1")
+					doQuery(myConnection, "UPDATE Portfolio SET d_bought_at = '" + date + "' WHERE stock = '" + ticker + "'")
 
-		except(pymysql.IntegrityError, pymysql.InternalError):
-			self.resultDialog("This is not a valid stock ticker. Please try again")
+					self.resultDialog("The stock has been successfully purchased. "
+									  "You're new account balance is: '" + str(balance) + "')")
+
+				except pymysql.InternalError:
+					self.resultDialog("This is not a valid stock ticker. Please try again.")
+
+				except pymysql.err.IntegrityError:
+					cur_volume = doQuery(myConnection, "SELECT volume FROM portfolio WHERE stock = '" + ticker + "'")
+					new_volume = int(volume) + int(cur_volume[0][0])
+					doQuery(myConnection, "UPDATE Portfolio SET volume = '" + str(new_volume) + "' WHERE stock = '" + ticker + "'")
+					doQuery(myConnection, "UPDATE Users SET current_balance = '" + str(balance) + "'WHERE user_id = 1")
+					doQuery(myConnection, "UPDATE Portfolio SET d_bought_at = '" + date + "' WHERE stock = '" + ticker + "'")
+					self.resultDialog("The stock has been successfully purchased. You're new account balance is: '" + str(balance) + "')")
+
+				except ValueError:
+					self.resultDialog("Please enter valid numbers for both price and volume.")
+
+			else:
+				self.resultDialog("Insufficient funds to buy this many shares. Please try again.")
+		else:
+			self.resultDialog("This is not a valid ticker. Please try again.")
 
 	def resultDialog(self, result):
 		dialog = BuyResultWindow(result, self)
+		dialog.show()
+		self.hide()
+
+class SellResultWindow(QDialog):
+	def __init__(self, result, parent=None):
+		super(SellResultWindow, self).__init__(parent)
+		self.grid = QGridLayout()
+		self.grid.addWidget(QLabel("Stock Selling"), 0, 0)
+		self.log = QTextEdit()
+		self.log.setText(str(result))
+		self.log.setReadOnly(True)
+		self.grid.addWidget(self.log, 1, 0)
+		self.button = QPushButton("Return To Homepage")
+		self.button.clicked.connect(self.handleClick)
+		self.button2 = QPushButton("Try Again")
+		self.button2.clicked.connect(self.tryClick)
+		self.grid.addWidget(self.button)
+		self.grid.addWidget(self.button2)
+		self.setLayout(self.grid)
+
+	def handleClick(self, event):
+		self.hide()
+		return
+
+	# button to go back to input window if needed (i.e. a typo is made by user)
+	def tryClick(self, event):
+		self.hide()
+		self.sellwindow = SellWindow()
+		self.sellwindow.show()
+		return self.sellwindow
+
+# Window for selling stock
+class SellWindow(QDialog):
+	def __init__(self, parent=None):
+		super(SellWindow, self).__init__(parent)
+		self.grid = QGridLayout()
+		cur_balance = doQuery(myConnection, "SELECT current_balance FROM users WHERE user_id = 1")
+		cur_balance = str(cur_balance[0][0])
+		self.log = QLineEdit()
+		self.log.setText("Current Balance: '" + cur_balance + "'")
+		self.log.setReadOnly(True)
+		self.label = QLabel("Sell Stock:")
+		self.vbox1 = QVBoxLayout()
+		self.vbox1.addWidget(self.label)
+		self.vbox1.addWidget(self.log)
+		self.vbox1.addWidget(QLabel("Enter Ticker of Stock to Sell:"))
+		self.textfield = QTextEdit()
+		self.vbox1.addWidget(self.textfield)
+		self.vbox1.addWidget(QLabel("Enter Price Sold at:"))
+		self.textfield1 = QTextEdit()
+		self.vbox1.addWidget(self.textfield1)
+		self.vbox1.addWidget(QLabel("Enter Number of Shares Sold"))
+		self.textfield2 = QTextEdit()
+		self.vbox1.addWidget(self.textfield2)
+		self.vbox1.addWidget(QLabel("Enter Selling Date:"))
+
+		# date field
+		cur_date = QDate(datetime.datetime.now())
+		self.date = QDateEdit(cur_date)
+		self.vbox1.addWidget(self.date)
+		enterbutton = QPushButton("Enter")
+		enterbutton.clicked.connect(self.handleClick)
+		self.vbox1.addWidget(enterbutton)
+		self.setLayout(self.vbox1)
+		self.show()
+
+	def handleClick(self, event):
+		ticker = self.textfield.toPlainText()
+		price = self.textfield1.toPlainText()
+		volume = self.textfield2.toPlainText()
+		date = self.date.date()
+		pydate = date.toPyDate()
+		date = pydate.strftime('%Y/%m/%d')
+
+		if doQuery(myConnection, "SELECT stock FROM stocks WHERE stock = '" + ticker + "'"):
+			cur_balance = doQuery(myConnection, "SELECT current_balance FROM users WHERE user_id = 1")
+			balance = int(cur_balance[0][0]) + (int(price) * int(volume))
+			if doQuery(myConnection, "SELECT volume FROM portfolio WHERE stock = '" + ticker + "'"):
+				cur_volume = doQuery(myConnection, "SELECT volume FROM portfolio WHERE stock = '" + ticker + "'")
+				cur_volume = int(cur_volume[0][0])
+				if cur_volume >= int(volume):
+					volume = cur_volume - int(volume)
+					try:
+						doQuery(myConnection, "UPDATE users SET current_balance = '" + str(balance) + "'WHERE user_id = 1")
+						doQuery(myConnection, "UPDATE portfolio SET volume = '" + str(volume) + "' WHERE stock = '" + ticker + "'")
+						self.resultDialog("The stock has been successfully sold. You're new account balance is: '" + str(balance) + "'")
+
+					except pymysql.err.DataError:
+						self.resultDialog("You do not own enough shares to sell this many. Please try again.")
+					except ValueError:
+						self.resultDialog("Please enter valid numbers for both price and volume.")
+				else:
+					self.resultDialog("You do not own enough shares to sell this many. Please try again.")
+			else:
+				self.resultDialog("You do not own this stock yet. Feel free to purchase it on the homepage.")
+		else:
+			self.resultDialog("This is not a valid ticker. Please try again.")
+
+	def resultDialog(self, result):
+		dialog = SellResultWindow(result, self)
 		dialog.show()
 		self.hide()
 
@@ -265,7 +382,9 @@ class Window(QWidget):
 		return self.buywindow
 
 	def handleSell(self, event):
-		return
+		self.sellwindow = SellWindow()
+		self.sellwindow.show()
+		return self.sellwindow
 
 	def handleRefresh(self, event):
 		lastDate = getRecentDate()
